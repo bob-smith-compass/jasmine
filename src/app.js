2c7cae877e6e3442874e31387adbc87fd98dd90f
David Shams <david.shams@gmail.com>
2019-01-07 13:46:28 -0600

    creating AngularJS Controller to be Unit Tested

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ angular.module('app', [])
 .controller('PasswordController', function PasswordController($scope) {
   $scope.password = '';
   $scope.grade = function() {
+    alert('strenght');
     var size = $scope.password.length;
     if (size > 8) {
       $scope.strength = 'strong';
